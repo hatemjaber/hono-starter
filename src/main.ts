6129900b6c9c48349f64ee79ce257d50ed75485c
Hatem Jaber <hatem@7079.net>
2024-12-30 09:24:31 -0500

    add main with tsconfig and pakcage update

---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { compress } from 'hono/compress';
+import { cors } from 'hono/cors';
+import { showRoutes } from 'hono/dev';
+import { logger as httpLogger } from 'hono/logger';
+import { trimTrailingSlash } from 'hono/trailing-slash';
+import { env } from './utils/env.js';
+import { logger } from './utils/logger.js';
+
+const app = new Hono();
+
+// Generic middlewares
+app.use(cors());
+app.use(compress());
+app.use(httpLogger());
+app.use(trimTrailingSlash());
+
+
+if (env.NODE_ENV === "development") {
+    console.log('Available routes:');
+    showRoutes(app);
+}
+
+const server = serve({ fetch: app.fetch, port: env.PORT });
+
+process.on('SIGTERM', () => {
+    logger.info('SIGTERM signal received');
+
+    logger.info('Closing http server');
+    server.close(async () => {
+        logger.info('Exiting...');
+        process.exit(0);
+    });
+});
